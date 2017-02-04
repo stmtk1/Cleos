621e030ebefa989cf4887b72f7eca9577a8870bf
stmtk <13043069@gakushuin.ac.jp>
2017-02-04 17:49:32 +0900

    関数をまとめた

--- C.java
+++ C.java
@@ -13,29 +13,43 @@ class C{
     String input;
     LinkedList<String> tags;
     DefaultStyledDocument doc;
-    MutableAttributeSet include, define;
+    MutableAttributeSet include, define, func;
     int last_end;
+    boolean in_function;
     
     public C(DefaultStyledDocument doc){
+        in_function = false;
         last_end = 0;
         this.doc = doc;
         include = new SimpleAttributeSet();
         StyleConstants.setForeground(include, new Color(255, 0, 0));
         define = new SimpleAttributeSet();
         StyleConstants.setForeground(define, new Color(0, 255, 0));
+        func = new SimpleAttributeSet();
+        StyleConstants.setForeground(func, new Color(0, 0, 255));
         try{
             input = doc.getText(0, doc.getLength() - 1);
-            trimInclude();
-            trimDefine();
+            parse();
         }catch(BadLocationException e){
             e.printStackTrace();
         }
     }
+
+    void parse(){
+        
+        for(int i = 0; i < 10; i++){
+       // while(input.length() != 0){
+            trimInclude();
+            trimDefine();
+            trimFuncPreDef();
+            trimFuncStart();
+        }
+    }
     
     void trimInclude(){
-        Pattern pattern = Pattern.compile("^\\s*#include\\p{Space}*[<\\\"]\\p{Alnum}+\\.h[>\\\"]");
+        Pattern pattern = Pattern.compile("\\A\\s*#include\\p{Space}*[<\\\"]\\p{Alnum}+\\.h[>\\\"]");
         Matcher matcher = pattern.matcher(input);
-        while(matcher.find()){
+        if(matcher.find()){
             doc.setCharacterAttributes(last_end + matcher.start(), matcher.group().length(), include, false);
             input = input.substring(matcher.end());
             last_end += matcher.end();
@@ -44,12 +58,34 @@ class C{
     }
     
     void trimDefine(){
-        Pattern pattern = Pattern.compile("^\\s*#define\\p{Space}+[\\p{Upper}\\p{Digit}]+\\p{Space}+\\p{Digit}+(\\.\\p{Digit}+)?");
+        Pattern pattern = Pattern.compile("\\A\\s*#define\\p{Space}+[\\p{Upper}\\p{Digit}]+\\p{Space}+\\p{Digit}+(\\.\\p{Digit}+)?");
         Matcher matcher = pattern.matcher(input);
-        while(matcher.find()){
+        if(matcher.find()){
             doc.setCharacterAttributes(last_end + matcher.start(), matcher.group().length(), define, false);
             input = input.substring(matcher.end());
             last_end += matcher.end();
+            matcher = pattern.matcher(input);
+        }
+    }
+    
+
+    void trimFuncStart(){
+        Pattern pattern = Pattern.compile("\\A\\s*\\p{Alpha}+\\p{Space}+\\p{Alpha}\\p{Alnum}*\\(\\)\\{");
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.find()){
+            doc.setCharacterAttributes(last_end + matcher.start(), matcher.group().length(), func, false);
+            input = input.substring(matcher.end());
+            matcher = pattern.matcher(input);
+            in_function = true;
+        }
+    }
+    
+    void trimFuncPreDef(){
+        Pattern pattern = Pattern.compile("\\A\\s*\\p{Alpha}+\\p{Space}+\\p{Alpha}\\p{Alnum}*\\(\\);");
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.find()){
+            System.out.println(matcher.group());
+            input = input.substring(matcher.end());
             matcher = pattern.matcher(input);
         }
     }
